0ab696163cc90e906fcde3d2d7f219e8f3331d2c
Mharysol <mharysol32@hotmail.com>
2022-10-12 23:03:54 -0300

    Hola Mundo

---
+++ Documents/Curso-JavaScript/Primer-Proyecto-deNode/index.js
@@ -0,0 +1,10 @@
+
+//este es el mensaje de bievenida
+console.log ("esta es la puerta de entrada al proyecto");
+/* Esta es la puerta
+intermedia, vamos a la 
+ultima linea de codigo
+*/
+
+//este es el mensaje para despedir
+console.log ("Adios");
