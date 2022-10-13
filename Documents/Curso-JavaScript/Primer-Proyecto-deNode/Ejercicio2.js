7315d7698ec07ace16d0b18407b10dc64fa3debe
Mharysol <mharysol32@hotmail.com>
2022-10-13 01:03:31 -0300

    Segundo Ejercicio

---
+++ Documents/Curso-JavaScript/Primer-Proyecto-deNode/Ejercicio2.js
@@ -0,0 +1,12 @@
+
+//Ejercicio 2
+
+const cumple = new Date(1989,06,23)+1
+const book={
+    titulo: "el sutil arte de que te importe un carajo",
+    autor: "Mark Manson",
+    fecha: 2020
+}
+
+const lista=["Sol", 32, true, cumple, book]
+console.log(lista)
